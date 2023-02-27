7b72b8644358e6e1f6fd04eb01b18e46c1a9c5cd
arkan-leki <arkan.leki@gmail.com>
2023-02-27 15:15:46 +0300

    main

---
+++ src/components/BottomNav.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useState } from 'react'
+import { FaCoffee, FaIceCream } from 'react-icons/fa';
+import { ItemsContext } from "../context/ItemsContext";
+
+const BottomNav = () => {
+    const { groups, setItemsGroup } = useContext(ItemsContext)
+
+    const [activeNavItem, setActiveNavItem] = useState('Home');
+
+    const handleItemClick = (item) => {
+        setActiveNavItem(item);
+        setItemsGroup(item)
+    };
+
+    return (
+        <nav className="fixed bottom-0 w-full bg-gray-900">
+            <ul className="flex flex-row justify-center gap-x-2 py-1 px-2">
+                {groups.map(group => (
+                    <li
+                        key={group}
+                        className={activeNavItem === group ? 'text-gray-300 hover:text-white cursor-pointer bg-slate-700 w-3/4 py-3 text-center text-lg' : 'text-gray-300 hover:text-white cursor-pointer w-3/4 py-3 text-center text-lg'}
+                        onClick={() => handleItemClick(group)}
+                    >
+                        {group === 'Cold' && <FaIceCream size={28} className='mx-auto' />}
+                        {group === 'Hot' && <FaCoffee size={28} className='mx-auto' />}
+                    </li>
+                ))}
+
+            </ul>
+        </nav>
+    )
+}
+
+export default BottomNav
