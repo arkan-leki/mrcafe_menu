addc8d7615d1b42739d3082b637d03afabe99b4d
arkan-leki <arkan.leki@gmail.com>
2023-02-27 16:45:40 +0300

    soacial

--- src/components/SocialBar.jsx
+++ src/components/SocialBar.jsx
@@ -5,10 +5,10 @@ const SocialBar = () => {
   return (
     <nav className="flex flex-row justify-around p-3 my-3  bg-[#f7f7f7]">
       <ul className='flex flex-row list-none m-0 p-0'>
-        <li className='mr-3'><button className='text-[#333] text-2xl'><FaFacebook color='blue' size={40} /></button></li>
-        <li className='mr-3'><button className='text-[#333] text-2xl'><FaSnapchat color='black' size={40} /></button></li>
-        <li className='mr-3'><button className='text-[#333] text-2xl'><FaInstagram color='purple' size={40} /></button></li>
-        <li className='mr-3'><button className='text-[#333] text-2xl'><FaTiktok color='black' size={40} /></button></li>
+        <li className='mr-3'><a href='https://www.facebook.com/mr.cafekalar' className='text-[#333] text-2xl'><FaFacebook color='blue' size={40} /></a></li>
+        <li className='mr-3'><a href='https://www.snapchat.com/add/mr.cafekalar' className='text-[#333] text-2xl'><FaSnapchat color='black' size={40} /></a></li>
+        <li className='mr-3'><a href='https://www.instagram.com/mr.cafekalar/' className='text-[#333] text-2xl'><FaInstagram color='purple' size={40} /></a></li>
+        <li className='mr-3'><a href='https://www.tiktok.com/@mr.cafe4' className='text-[#333] text-2xl'><FaTiktok color='black' size={40} /></a></li>
       </ul>
     </nav>
   )
